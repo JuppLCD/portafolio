13cc2c5eb7e3ef5267dc448c5e77802be63f3dfe
JuppLCD <lorenzocanovas207@gmail.com>
2022-06-10 17:11:11 -0300

    Añadiendo modal y actualizando informacion

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const proyectInfo = [
+	{
+		title: 'Harry-Potter',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nemo sint. Doloribus molestias quis obcaecati culpa, nemo iusto cum eum illo laudantium commodi suscipit sequi adipisci inventore, officiis ducimus nesciunt, modi velit voluptate ipsam nisi excepturi neque ipsa perferendis! Consectetur repellendus recusandae vitae nostrum, tempora, illum repellat eum earum consequatur aspernatur, doloribus corrupti. <br/> Eveniet qui asperiores temporibus sint, inventore doloremque esse voluptate nostrum quia ducimus dicta incidunt porro placeat architecto quas? Iure beatae officia necessitatibus mollitia aspernatur eveniet perspiciatis, inventore natus! Nemo, blanditiis magni. Magni, distinctio quasi temporibus tempore perspiciatis laborum. Non accusamus aut consectetur excepturi recusandae quasi, sed velit veniam tenetur voluptates hic? Alias ut perferendis provident, reprehenderit, sit aliquid nisi culpa iusto et optio officiis eum debitis hic.',
+		demo: 'https://harry-potter-react-ts.netlify.app/',
+		code: 'https://github.com/JuppLCD/Harry-Potter-ReactTS',
+		technologies: 'Web App, React, Typescript, api de Harry Potter',
+	},
+	{
+		title: 'ToDo-Fontend',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nemo sint. Doloribus molestias quis obcaecati culpa, nemo iusto cum eum illo laudantium commodi suscipit sequi adipisci inventore, officiis ducimus nesciunt, modi velit voluptate ipsam nisi excepturi neque ipsa perferendis! Consectetur repellendus recusandae vitae nostrum, tempora, illum repellat eum earum consequatur aspernatur, doloribus corrupti. Eveniet qui asperiores temporibus sint, inventore doloremque esse voluptate nostrum quia ducimus dicta incidunt porro placeat architecto quas? Iure beatae officia necessitatibus mollitia aspernatur eveniet perspiciatis, inventore natus! Nemo, blanditiis magni. Magni, distinctio quasi temporibus tempore perspiciatis laborum. Non accusamus aut consectetur excepturi recusandae quasi, sed velit veniam tenetur voluptates hic? Alias ut perferendis provident, reprehenderit, sit aliquid nisi culpa iusto et optio officiis eum debitis hic.',
+		demo: 'https://todo-react-crud.herokuapp.com/',
+		code: 'https://github.com/JuppLCD/react-todo',
+		technologies: 'Web App, React',
+	},
+	{
+		title: 'ToDo-Backend',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nemo sint. Doloribus molestias quis obcaecati culpa, nemo iusto cum eum illo laudantium commodi suscipit sequi adipisci inventore, officiis ducimus nesciunt, modi velit voluptate ipsam nisi excepturi neque ipsa perferendis! Consectetur repellendus recusandae vitae nostrum, tempora, illum repellat eum earum consequatur aspernatur, doloribus corrupti. Eveniet qui asperiores temporibus sint, inventore doloremque esse voluptate nostrum quia ducimus dicta incidunt porro placeat architecto quas? Iure beatae officia necessitatibus mollitia aspernatur eveniet perspiciatis, inventore natus! Nemo, blanditiis magni. Magni, distinctio quasi temporibus tempore perspiciatis laborum. Non accusamus aut consectetur excepturi recusandae quasi, sed velit veniam tenetur voluptates hic? Alias ut perferendis provident, reprehenderit, sit aliquid nisi culpa iusto et optio officiis eum debitis hic.',
+		demo: 'https://todo-react-crud.herokuapp.com/',
+		code: 'https://github.com/JuppLCD/todo-app-backend',
+		technologies: 'NodeJS, Express, MongoDB',
+	},
+	{
+		title: 'Rick-and-Morty',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nemo sint. Doloribus molestias quis obcaecati culpa, nemo iusto cum eum illo laudantium commodi suscipit sequi adipisci inventore, officiis ducimus nesciunt, modi velit voluptate ipsam nisi excepturi neque ipsa perferendis! Consectetur repellendus recusandae vitae nostrum, tempora, illum repellat eum earum consequatur aspernatur, doloribus corrupti. Eveniet qui asperiores temporibus sint, inventore doloremque esse voluptate nostrum quia ducimus dicta incidunt porro placeat architecto quas? Iure beatae officia necessitatibus mollitia aspernatur eveniet perspiciatis, inventore natus! Nemo, blanditiis magni. Magni, distinctio quasi temporibus tempore perspiciatis laborum. Non accusamus aut consectetur excepturi recusandae quasi, sed velit veniam tenetur voluptates hic? Alias ut perferendis provident, reprehenderit, sit aliquid nisi culpa iusto et optio officiis eum debitis hic.',
+		demo: 'https://rick-and-morty-jupplcd.netlify.app/',
+		code: 'https://github.com/JuppLCD/rick-and-morty',
+		technologies: 'Web App, React, api Rick and Morty',
+	},
+	{
+		title: 'Empresa',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, nemo sint. Doloribus molestias quis obcaecati culpa, nemo iusto cum eum illo laudantium commodi suscipit sequi adipisci inventore, officiis ducimus nesciunt, modi velit voluptate ipsam nisi excepturi neque ipsa perferendis! Consectetur repellendus recusandae vitae nostrum, tempora, illum repellat eum earum consequatur aspernatur, doloribus corrupti. Eveniet qui asperiores temporibus sint, inventore doloremque esse voluptate nostrum quia ducimus dicta incidunt porro placeat architecto quas? Iure beatae officia necessitatibus mollitia aspernatur eveniet perspiciatis, inventore natus! Nemo, blanditiis magni. Magni, distinctio quasi temporibus tempore perspiciatis laborum. Non accusamus aut consectetur excepturi recusandae quasi, sed velit veniam tenetur voluptates hic? Alias ut perferendis provident, reprehenderit, sit aliquid nisi culpa iusto et optio officiis eum debitis hic.',
+		demo: 'https://flamboyant-turing-e9b396.netlify.app/',
+		code: 'https://github.com/JuppLCD/redesign-web-empresa',
+		technologies: 'Web App, React',
+	},
+];
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import './sass/main.scss';
+import { proyectInfo } from './data';
 
 const portafolio = document.getElementById('portafolio');
+
+const modal = document.getElementById('proyectInfoModal');
+const template = document.getElementById('proyectInfoTemplate').content;
+
 portafolio.addEventListener('click', (e) => {
-	if (e.target.classList.contains('card-img') || e.target.classList.contains('card-img-overlay')) {
-		e.target.parentNode.querySelector('.card-img-overlay').classList.toggle('active');
+	if (
+		e.target.tagName === 'ARTICLE' ||
+		e.target.parentNode.tagName === 'ARTICLE' ||
+		e.target.parentNode.parentNode.tagName === 'ARTICLE' ||
+		e.target.parentNode.parentNode.parentNode.tagName === 'ARTICLE'
+	) {
+		let articleElement = e.target;
+		while (articleElement.tagName !== 'ARTICLE') {
+			articleElement = articleElement.parentNode;
+		}
+		const fragment = document.createDocumentFragment();
+
+		const titleProyect = articleElement.dataset.proyect;
+		const data = proyectInfo.filter((proyect) => proyect.title === titleProyect)[0];
+
+		template.querySelector('.modal-title span').textContent = data.title;
+		template.querySelector('.modal-body h6').textContent = data.technologies;
+		template.querySelector('.modal-body p').innerHTML = data.description;
+		template.querySelector('.modal-body a.btn-warning').href = data.demo;
+		template.querySelector('.modal-body a.btn-light').href = data.code;
+
+		const clone = template.cloneNode(true);
+		fragment.appendChild(clone);
+
+		modal.querySelector('.modal-dialog').innerHTML = '';
+		modal.querySelector('.modal-dialog').appendChild(fragment);
 	}
 });
