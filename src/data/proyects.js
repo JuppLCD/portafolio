90a72e82e8b2f7bea748e84876b7b4ae302f8eb1
JuppLCD <lorenzocanovas207@gmail.com>
2023-01-24 20:35:35 -0300

    Create sections in compontns/Sections

---
+++ src/data/proyects.js
@@ -0,0 +1,37 @@
+export const proyectsData = [
+	{
+		title: 'Movies Frontend',
+		info: 'Web App | React | Typescript | Bootstrap 5 | Api de TheMovieDB | NodeJS | Sass',
+		img: 'Movies.png',
+	},
+	{
+		title: 'Movies Backend',
+		info: 'NodeJS | Express | hapi/boom | MySQL | Sequelize | User',
+		img: 'backend-img.png',
+	},
+	{
+		title: 'Harry Potter',
+		info: 'Web App | React | Typescript | Api de Harry Potter',
+		img: 'Harry-Pooter-WebApp.png',
+	},
+	{
+		title: 'Meme Generator',
+		info: 'Web App | React | Typescript | Api Memes | Api Backend NodeJS | TailwindCSS',
+		img: 'Memes-ReactJS.png',
+	},
+	{
+		title: 'Meme Generator Backend',
+		info: 'NodeJS | Express | hapi/boom | MySQL | Sequelize | User session | Typescript | Firebase Storage | Multer',
+		img: 'backend-img.png',
+	},
+	{
+		title: 'Rick and Morty',
+		info: 'Web App | React | Api Rick and Morty',
+		img: 'Rick-Morty-WebApp.png',
+	},
+	{
+		title: 'Chat App Sockets',
+		info: 'PHP | Ratchet | MySQL | User',
+		img: 'Chat-App-PHP.jpg',
+	},
+];
---
+++ src/data/skills.js
@@ -0,0 +1,47 @@
+export const skillData = [
+	{
+		skill: 'HTLM',
+		color: '#e34c26',
+		icon: 'fa-html5',
+	},
+	{
+		skill: 'Javascript',
+		color: '#f0db4f',
+		icon: 'fa-js',
+	},
+	{
+		skill: 'CSS',
+		color: '#264de4',
+		icon: 'fa-css3-alt',
+	},
+	{
+		skill: 'Sass',
+		color: '#cc6699',
+		icon: 'fa-sass',
+	},
+	{
+		skill: 'Bootstrap',
+		color: '#563d7c',
+		icon: 'fa-bootstrap',
+	},
+	{
+		skill: 'React',
+		color: '#61dbfb',
+		icon: 'fa-react',
+	},
+	{
+		skill: 'Node JS',
+		color: '#68a063',
+		icon: 'fa-node-js',
+	},
+	{
+		skill: 'Git',
+		color: '#f34f29',
+		icon: 'fa-git-alt',
+	},
+	{
+		skill: 'PHP',
+		color: '#8892be',
+		icon: 'fa-php',
+	},
+];
